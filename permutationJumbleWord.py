762c666b25291f5733b08ca7ecef92c343013b6b
manishkumar <manishkumar13may2003@gmail.com>
2023-08-13 00:00:25 +0530

    commit

--- permutationJumbleWord.py
+++ permutationJumbleWord.py
@@ -1,3 +1,18 @@
+import random
+def choose():worsd['rainbow','computer','science','programming','mathematics','player','condition','reverse','water','board']
+pick=random.choice(words)
+return pick
+
+def jumble(words):
+    jumbled="".join(random.sample(word,len(word)))
+    return jumbled
+
+    def thank(p1n,p2n,p1,p2):
+        print(p1n,'your score is:'p1)
+        print(p2n,'your score is:'p2)
+        print('Thanks for playing')
+        print('Have a nice day')
+
 def play():
      plname=input('Player1,Please enter your name')
      p2name=input('Player2,Please enter your name')
@@ -13,9 +28,9 @@ def play():
         if turn%2=0:
             print(p1name,'Your turn.')
             ans=input('What is on my mind?')
-            if ans =picked_words:
+            if ans==picked_words:
                 ppl=ppl+1
-                print('Your score is:'ppl)
+                print('Your score is:',ppl)
             else:
                 print('Better luck next time,I thought:',picked_word)
             c=input('Press 1 to continue and 0 to quit :')
@@ -24,13 +39,13 @@ def play():
                 break
             #player2
         else:
-            print(p1name,'Your turn.')
+            print(p2name,'Your turn.')
             ans=input('What is on my mind?')
-            if ans =picked_words:
-                ppl=ppl+1
-                print('Your score is:'ppl)
+            if ans==picked_words:
+                pp2=pp2+1
+                print('Your score is:',ppl)
             else:
                 print('Better luck next time,I thought:',picked_word)
-            c=0:
-            thank(p1name,p2name,pp1,pp2)
-            break
+            c=input('Press 1 to continue and 0mto quit :')
+            
+        
