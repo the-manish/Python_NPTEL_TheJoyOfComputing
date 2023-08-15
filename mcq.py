2d353022536b545218806f6d3b9baf9fd18dd998
manishkumar <manishkumar13may2003@gmail.com>
2023-08-15 15:28:50 +0530

    first commit

---
+++ mcq.py
@@ -0,0 +1,24 @@
+s = "hello"
+print(s[::-1])
+
+
+a = 5
+b = 2
+print(a // b)
+
+
+a = 10
+b = 5
+c = a % b
+print(c)
+
+s = "python"
+print(s[1:4])
+
+L = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+print(L[2:5])
+
+ #c=b**3
+#print(c)
+#through an error!
