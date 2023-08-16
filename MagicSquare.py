13771bab3226566ba030a62a88fc6de514c68494
manishkumar <manishkumar13may2003@gmail.com>
2023-08-16 23:29:24 +0530

    first commit

---
+++ MagicSquare.py
@@ -0,0 +1,18 @@
+def magic_square(n):
+    
+    magicSquare=[]
+    for i in range (n):
+        l=[]
+        for j in range(n):
+            l.append(0)
+        magicSquare.append(l)
+
+    for i in range (n):
+        
+        for j in range(n):
+
+         print(magicSquare[i][j],end="")
+         print()
+         
+
+magic_square(3)
