4bf26136e8fd6137d3c7479fd56ab54020887183
manishkumar <manishkumar13may2003@gmail.com>
2023-08-20 23:26:46 +0530

    first commit

---
+++ Birthday_Paradox.py
@@ -0,0 +1,5 @@
+#Most of the years that can be divided by 4 are leap years.
+#Exception:Century years are not leap years unless they are divided by 400.
+#if year is divisible by 400 then is_leap_year elseif year is divisible by100 then not_leap_year
+#else if year is divisible by 4 then is_leap_year
+#else not_leap_year
