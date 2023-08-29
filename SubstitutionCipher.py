4131dc283957528805754dbed3ed39ddb6538ec8
manishkumar <manishkumar13may2003@gmail.com>
2023-08-29 10:44:55 +0530

    first commit

--- SubstitutionCipher.py
+++ SubstitutionCipher.py
@@ -3,3 +3,14 @@ dict={}
 for i in range(len(string.ascii_letters)):
     dict[string.ascii_letters[i]]=string.ascii_letters[i-1]
 print(dict)
+with open("ip-file.txt") as f:
+    while True:
+        c=f.read()
+        if not c:
+            print("End of file")
+            break
+        if c in dict:
+            data=dict[c]
+        else:
+            data=c
+        print(data)
